8692d753d1d6e7f34e4b77ae144bb86b8a570252
Regis Philibert <login@regisphilibert.com>
2018-08-20 11:21:59 -0400

    Delete /public

--- public/scripts/global.min.be235b8b5fe3752289d918da30da31909babfcb53876e567e86f762958542df3.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){$('body').addClass('js-ok')
-if(typeof weather_location!=="undefined"&&typeof cloudinary!=="undefined"){console.log({cloudinary:cloudinary,location:weather_location})}});
